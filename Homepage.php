2e08408c45cceed32b6d6503a171cf309c5012ee
Julia Werkoff <jwerkoff@gmail.com>
2016-01-12 14:44:05 +0100

    add navbar to the homepage

--- Homepage.php
+++ Homepage.php
@@ -2,12 +2,40 @@
 
 <html>
     <head>
-        <title>TODO supply a title</title>
+        <title>Cercle d'escrime de Massy - Accueil</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+        <link href="CSS/home.css" rel="stylesheet" media="all" type="text/css">
     </head>
     <body>
-        <div>TODO write content</div>
+       <nav class="navbar navbar-default navbar-static-top">
+            <div class="container-fluid">
+               <div class="navbar-header">
+                   <a class="navbar-brand" href="#">
+                       <img alt="Logo CEM" src="images/LogoCem.png">
+                   </a>
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                      <span class="sr-only">Toggle navigation</span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">CEM</a>
+                </div>
+
+              <!-- Collect the nav links, forms, and other content for toggling -->
+              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a href="#">Evenements <span class="sr-only">(current)</span></a></li>
+                  <li><a href="#">Photos</a></li>
+                  <li><a href="#">Contact</a></li>
+                </ul> 
+              </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid --> 
+           
+        </nav>  
+        
+        
     </body>
 </html>
