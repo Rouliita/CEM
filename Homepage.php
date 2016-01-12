ca5145850c2ebe2f9d364c444015d3f454465a69
Julia Werkoff <jwerkoff@gmail.com>
2016-01-12 15:05:11 +0100

    modification css for navbar

--- Homepage.php
+++ Homepage.php
@@ -21,7 +21,6 @@
                       <span class="icon-bar"></span>
                       <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="#">CEM</a>
                 </div>
 
               <!-- Collect the nav links, forms, and other content for toggling -->
