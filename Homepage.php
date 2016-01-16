8c937e34301d818571d7381600d710be3ac782fb
Julia Werkoff <jwerkoff@gmail.com>
2016-01-16 17:23:04 +0100

    add header, jQuery and carrousel o the project

--- Homepage.php
+++ Homepage.php
@@ -5,16 +5,21 @@
         <title>Cercle d'escrime de Massy - Accueil</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="jQuery/jquery-1.12.0.min.js"></script>
         <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
         <link href="CSS/home.css" rel="stylesheet" media="all" type="text/css">
     </head>
     <body>
-       <nav class="navbar navbar-default navbar-static-top">
+        <div id="header">
+            <a href="#">
+                <img alt="Logo CEM" src="images/LogoCem.png" class="img-circle">
+            </a>
+        </div>
+        <p> Cercle d'escrime de Massy </p>
+       <nav class="navbox navbar navbar-default navbar-static-top">
             <div class="container-fluid">
                <div class="navbar-header">
-                   <a class="navbar-brand" href="#">
-                       <img alt="Logo CEM" src="images/LogoCem.png">
-                   </a>
+                   
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                       <span class="sr-only">Toggle navigation</span>
                       <span class="icon-bar"></span>
@@ -32,9 +37,43 @@
                 </ul> 
               </div><!-- /.navbar-collapse -->
             </div><!-- /.container-fluid --> 
-           
         </nav>  
         
-        
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+              <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+              <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+              <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+              <div class="item active">
+                <img src="images/test1.jpg" alt="test">
+                <div class="carousel-caption">
+                  ...
+                </div>
+              </div>
+              <div class="item">
+                  <img src="images/test2.jpg" alt="test2">
+                <div class="carousel-caption">
+                  ...
+                </div>
+              </div>
+              ...
+            </div>
+
+            <!-- Controls -->
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+              <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+              <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+        </div>
+        <script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
     </body>
 </html>
