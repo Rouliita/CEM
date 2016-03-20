60bb7e177f935198b4ac8c5408238d2046972326
Axel Venet <kern046@gmail.com>
2016-03-20 17:56:20 +0100

    Refactor the footer to be displayed with flex directives

--- index.php
+++ index.php
@@ -10,31 +10,32 @@
         <link href="CSS/home.css" rel="stylesheet" media="all" type="text/css">
     </head>
     <body>
-       <nav class="navbox navbar navbar-default navbar-static-top">
-            <div class="container-fluid">
-               <div class="navbar-header">
-                   <a href="#">
-                    <img alt="Logo CEM" src="images/LogoCem.png" class="img-circle">
-                    </a>
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                      <span class="sr-only">Toggle navigation</span>
-                      <span class="icon-bar"></span>
-                      <span class="icon-bar"></span>
-                      <span class="icon-bar"></span>
-                    </button>
-                </div>
+        <div id="header">
+            <nav class="navbox navbar navbar-default navbar-static-top">
+                 <div class="container-fluid">
+                    <div class="navbar-header">
+                        <a href="#">
+                         <img alt="Logo CEM" src="images/LogoCem.png" class="img-circle">
+                         </a>
+                         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                           <span class="sr-only">Toggle navigation</span>
+                           <span class="icon-bar"></span>
+                           <span class="icon-bar"></span>
+                           <span class="icon-bar"></span>
+                         </button>
+                     </div>
 
-              <!-- Collect the nav links, forms, and other content for toggling -->
-              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li><a href="#">Evenements <span class="sr-only">(current)</span></a></li>
-                  <li><a href="#">Photos</a></li>
-                  <li><a href="#">Contact</a></li>
-                </ul> 
-              </div><!-- /.navbar-collapse -->
-            </div><!-- /.container-fluid --> 
-        </nav>  
-        
+                   <!-- Collect the nav links, forms, and other content for toggling -->
+                   <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                     <ul class="nav navbar-nav">
+                         <li><a href="#">Evenements <span class="sr-only">(current)</span></a></li>
+                       <li><a href="#">Photos</a></li>
+                       <li><a href="#">Contact</a></li>
+                     </ul> 
+                   </div><!-- /.navbar-collapse -->
+                 </div><!-- /.container-fluid --> 
+             </nav>
+        </div>
         <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
             <!-- Indicators -->
             <ol class="carousel-indicators">
@@ -73,21 +74,33 @@
         
         <div id="footer">
             <h3>Partenaires :</h3>
-            <a href="http://www.ville-massy.fr/" target="_blank">
-                <img src="images/logo-massy.jpg" alt="logo ville de Massy" id="logo-footer">
-            </a> 
-            <a href="http://www.essonne.fr/" target="_blank">
-                <img src="images/Logo-Essonne.svg.png" alt="logo CG Essonne" id="logo-footer">
-            </a> 
-            <a href="http://www.escrime-ffe.fr/" target="_blank">
-                <img src="images/logo-FFE.png" alt="logo FFE" id="logo-footer">
-            </a> 
-            <a href="http://cde91.fr/" target="_blank">
-                <img src="images/logo_CDE.jpg" alt="logo CDE" id="logo-footer">
-            </a> 
-            <a href="http://ligue.leav.fr/" target="_blank">
-                <img src="images/Logo_leifo.png" alt="logo LEIFO" id="logo-footer">
-            </a> 
+            <ul>
+                <li>
+                    <a href="http://www.ville-massy.fr/" target="_blank">
+                        <img src="images/logo-massy.jpg" alt="logo ville de Massy" id="logo-footer">
+                    </a>
+                </li>
+                <li>
+                    <a href="http://www.essonne.fr/" target="_blank">
+                        <img src="images/Logo-Essonne.svg.png" alt="logo CG Essonne" id="logo-footer">
+                    </a> 
+                </li>
+                <li>
+                    <a href="http://www.escrime-ffe.fr/" target="_blank">
+                        <img src="images/logo-FFE.png" alt="logo FFE" id="logo-footer">
+                    </a> 
+                </li>
+                <li>
+                    <a href="http://cde91.fr/" target="_blank">
+                        <img src="images/logo_CDE.jpg" alt="logo CDE" id="logo-footer">
+                    </a> 
+                </li>
+                <li>
+                    <a href="http://ligue.leav.fr/" target="_blank">
+                        <img src="images/Logo_leifo.png" alt="logo LEIFO" id="logo-footer">
+                    </a> 
+                </li>
+            </ul>
         </div>
         <script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
     </body>
